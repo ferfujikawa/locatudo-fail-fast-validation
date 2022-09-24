00eee6c7e980d741765d9c0b34488f0218cdb3f2
Fernando Fujikawa <ferfujikawa@gmail.com>
2022-09-24 16:05:10 -0300

    Criação de classe de extensão da biblioteca Flunt.

---
+++ Locatudo.Compartilhado/Extensoes/FluntExtensions.cs
@@ -0,0 +1,26 @@
+﻿using Flunt.Validations;
+
+namespace Locatudo.Compartilhado.Extensoes
+{
+    public static class FluntExtensions
+    {
+        public static Contract<T> HasMinLength<T>(this Contract<T> contract, string val, int comparer, string key, string message)
+        {
+            if (string.IsNullOrEmpty(val) || val.Length < comparer)
+                contract.AddNotification(key, message);
+                
+            return contract;
+        }
+
+        public static Contract<T> HasMaxLength<T>(this Contract<T> contract, string val, int comparer, string key, string message)
+        {
+            if (string.IsNullOrEmpty(val))
+                return contract;
+
+            if (val.Length > comparer)
+                contract.AddNotification(key, message);
+
+            return contract;
+        }
+    }
+}
